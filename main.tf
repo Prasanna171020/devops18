e101d78f868b886c4b91cd4c5057705e5dd12e7a
PrasannaGandrothu <147391045+Prasanna171020@users.noreply.github.com>
2025-12-01 14:31:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0cae6d6fe6048ca2c"
+    image_id           = "ami-0fa3fe0fa7920f68e"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "minimajor"
+    key_name = "Mykeypair"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-097b4975cf1130493", "subnet-01ee7e5094f09f366"]
+     subnets = ["subnet-01a740f87cdcf8c67", "subnet-01b2ebc07128898fd"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
